5e711a219721fc7b90f377ada1cf1da2432f6bb5
Luna <luna.dragon@suse.com>
2023-05-21 09:41:01 +0530

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-
 import react from "@astrojs/react";
+import wasm from '@rollup/plugin-wasm';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), react()]
+  integrations: [tailwind(), image(), react()],
+  vite: {
+    plugins: [wasm()]
+  }
 });
